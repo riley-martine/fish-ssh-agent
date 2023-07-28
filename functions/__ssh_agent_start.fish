fd7b181062d44fdb7945c12f2334042b5396e57e
Riley Martine <riley.martine@protonmail.com>
2023-07-28 12:18:21 -0600

    Cleanup socket if no agent

--- functions/__ssh_agent_start.fish
+++ functions/__ssh_agent_start.fish
@@ -1,4 +1,8 @@
 function __ssh_agent_start -d "start a new ssh agent"
+  if test -e $SSH_AUTH_SOCK
+    rm -f $SSH_AUTH_SOCK
+    echo "removed $SSH_AUTH_SOCK"
+  end
   set ssh_env (ssh-agent -c -a "$SSH_AUTH_SOCK" | sed 's/^echo/#echo/')
   if test "$pipestatus[1]" -eq 0
       echo "$ssh_env" > $SSH_ENV
